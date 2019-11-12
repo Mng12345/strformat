09f6d3819465cb3ce7ddc56d8286fb33f73e50d2
Mng12345 <1570977353@qq.com>
2019-11-12 17:50:15 +0800

    1

---
+++ src/main/java/strformat/StrFormatter.java
@@ -0,0 +1,26 @@
+package strformat;
+
+/**
+ * @Author zhangming
+ * @Date 2019/11/12 16:11
+ */
+public class StrFormatter {
+
+    private StrFormatter() {}
+
+    private StringBuilder stringBuilder = new StringBuilder();
+
+    public static StrFormatter str() {
+        return new StrFormatter();
+    }
+
+    public StrFormatter i(Object object) {
+        this.stringBuilder.append(object);
+        return this;
+    }
+
+    public String toString() {
+        return this.stringBuilder.toString();
+    }
+
+}
